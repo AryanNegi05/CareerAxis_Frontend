a41964bc4b89e7327f750e76383b48c4f74e0bf5
AryanNegi05 <negiaryan13205@gmail.com>
2025-06-02 20:08:36 +0530

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:4000/api/v1';
+const API_BASE_URL = 'https://careeraxis-backend-1.onrender.com';
 
 const apiCall = async (endpoint, options = {}, token) => {
   try {
